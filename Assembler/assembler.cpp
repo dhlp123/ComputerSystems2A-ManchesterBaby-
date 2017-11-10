90cf07c4a4c0d55f1b6e943f700a372c16a8cc33
Matthew Foulis <matt@Matthews-MacBook-Pro.local>
2017-11-10 11:50:15 +0000

    Add function declerations

--- Assembler/assembler.cpp
+++ Assembler/assembler.cpp
@@ -2,9 +2,53 @@
 
 using namespace std;
 
+void increment_CI();
+void fetch();
+void decode();
+void execute();
+void display();
+
 int main()
 {
-	cout << "test" << endl;
+	cout << "start" << endl;
 
+	bool finish = false;
+	
+	while(!finish)
+	{
+		increment_CI();
+		fetch();
+		decode();
+		execute();
+		display();
+	}
+	
+	cout << "end" << endl;
 	return 0;
 }
+
+void increment_CI()
+{
+
+}
+
+void fetch()
+{
+
+}
+
+void decode()
+{
+
+}
+
+void execute()
+{
+
+}
+
+void display()
+{
+
+}
+

c8b7fbed8424d2d4ed589fcf4932a424df9bce42
Matthew Foulis <matt@Matthews-MacBook-Pro.local>
2017-11-10 12:34:39 +0000

    Add init and display functions

--- Assembler/assembler.cpp
+++ Assembler/assembler.cpp
@@ -2,29 +2,46 @@
 
 using namespace std;
 
+int p_Instruction[32];
+int c_Instruction[32];
+int accumulator[32];
+int store[32][32];
+
 void increment_CI();
 void fetch();
 void decode();
 void execute();
 void display();
+void init();
 
 int main()
 {
 	cout << "start" << endl;
-
+	init();
+	
 	bool finish = false;
 	
-	while(!finish)
-	{
+	//while(!finish)
+	//{
 		increment_CI();
 		fetch();
 		decode();
 		execute();
 		display();
-	}
+	//}
 	
 	cout << "end" << endl;
 	return 0;
+}
+
+void init()
+{
+	for(int i = 0; i < 32; i++)
+	{
+		c_Instruction[i] = 0;
+		p_Instruction[i] = 0;
+		accumulator[i] = 0;
+	}
 }
 
 void increment_CI()
@@ -49,6 +66,20 @@ void execute()
 
 void display()
 {
+	for(int i = 0; i < 32; i++)
+	{
+		for(int j = 0; j < 32; j++)
+		{
+			if(store[i][j] == 1)
+				cout << "1 ";
+			else
+				cout << "0 ";
+		}
+		cout << endl;
+	}
 
+	cout << "accumulator: " << endl;
+	cout << "control insturction: " << endl;
+	cout << "present insturction: " << endl;
 }
 

3c0b8df98d2f3d14528f7ac53f863840c9088392
Douglas P <dparkinson@dundee.ac.uk>
2017-11-11 21:34:56 +0000

    This is a commit for the skeletal code of the compiler program as well as it's header file.

---
+++ Compiler/compiler.cpp
@@ -0,0 +1,38 @@
+#include "compiler.h"
+
+string opcode[] = {};
+string opcodeBinary[] = {};
+
+//Reads the Assembly code in from a file
+//And puts it into an array of strings for each line.
+void readCode(){
+
+}
+
+//Goes through the code line by line and translates it to it's binary form.
+//This takes the opcode and the address and translates them to their binary forms.
+void translateCodeToBinary(){
+
+}
+
+//Takes the binary of each lines address and opcode.
+//Then it translates these into little endian form and puts it into a 32 bit integer.
+void translateBinaryToLine(){
+
+}
+
+//Takes the 32 bit integer and writes it into a line of the output file for the compiled code.
+//If there is no file avaiable it should create one and write to it.
+void writeMachineCode(){
+
+}
+
+
+//Main function for the methods calls to take place.
+int main(){
+
+	
+	return 0;
+}
+
+

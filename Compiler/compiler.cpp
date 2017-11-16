7d5ac3b7f1cd4c94b75d6beb1ddabf0e551e8fe0
Douglas P <dparkinson@dundee.ac.uk>
2017-11-16 21:40:06 +0000

    This should be the final version of the compiler with varaible referencing and declaration. I tested this with the example code mostly so it should be fine.

--- Compiler/compiler.cpp
+++ Compiler/compiler.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <sstream>
 #include <bitset>
 #include <fstream>
 #include <algorithm>
+#include <vector>
 
+#include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "compiler.h"
 
 using namespace std;
@@ -18,24 +21,135 @@ NOTE: Since I have gotten the big endian conversion for the opCodes and Address 
 
 string opcode[] = {"JMP", "JRP", "LDN", "STO", "SUB", "SUB", "CMP", "STP"};
 string opcodeBinary[] = {"000", "100", "010", "110", "001", "101", "011", "111"};
+signed int opLen = (sizeof(opcode)/sizeof(opcode[0]));
+
+
+vector<string> splitString(string str, string delim){
+	vector<string> toks;
+	stringstream ss(str);
+	string line;
+
+	while(getline(ss, line)){
+		size_t pos, prev = 0;
+		while((pos = line.find_first_of(delim, prev)) != string::npos){
+			if(pos > prev){
+				toks.push_back(line.substr(prev, pos-prev));
+			}
+			prev = pos+1;		
+		}
+		if(prev < line.length()){
+			toks.push_back(line.substr(prev, string::npos));
+		}	
+	}
+
+	return toks;
+}
+
 
 //Reads the Assembly code in from a file
 //Calls the translate methods for each line.
 void readCode(string fileName){
-	ifstream file(fileName.c_str());
+	ifstream file;
 
-	string opCode, num;
+	file.open(fileName.c_str());
 
-	while(file >> opCode >> num){
-		num.erase(remove(num.begin(), num.end(), ';'), num.end());
-		
-		int number = atoi(num.c_str());
+	string line;
+	int lineNum = 0;
+	vector<string> vars;
 
-		cout << opCode << " " << number << endl;
-		
-		translateCodeToBinary(opCode, number);
+
+	//Getting Varaible Name Declarations
+	while(getline(file, line)){
+
+		vector<string> toks;
+		stringstream ss; 
+
+		toks = splitString(line, " ");
+
+		if(toks[0].find(':') != string::npos && toks[1] == "VAR"){
+			stringstream ss;
+			ss << toks[0] << lineNum;
+			vars.push_back(ss.str());
+		}
+
+		if(toks[0] != ";"){ lineNum++; }
+
 	}
+	
+	file.close();
 
+	file.open(fileName.c_str());
+
+	cout << "Variables Loaded: " << vars.size() << endl;
+	cout << "Starting to Read Lines" << endl;
+
+	//Reading Lines
+	while(getline(file, line)){
+		
+		string opCode, num;
+		vector<string> toks;
+		stringstream ss;
+
+		toks = splitString(line, " :;");
+
+		for(unsigned int i=0; i<toks.size(); i++){
+
+			if(toks[i].find(':') != string::npos && toks[i+1] == "VAR"){
+				if(toks[i+2] != ""){
+					char * end;
+					long int number = strtol(toks[i+2].c_str(), &end, 0);
+
+					cout << toks[i]  << " " << toks[i+1] << " " << number << endl;
+
+					translateCodeToBinary(number);
+				}
+			}
+			else if(toks[i] == string("VAR")){
+				if(toks[i+1] != ""){
+					char * end;
+					long int number = strtol(toks[i+1].c_str(), &end, 0);
+
+					cout << toks[i]  << " " << number << endl;
+
+					translateCodeToBinary(number);
+				}
+			}
+			else{
+				for(int op=0; op<opLen; op++){
+					if(toks[i] == opcode[op]){
+						opCode = toks[i];
+						for(unsigned int v=0; v<vars.size(); v++){
+							if(toks[i+1] != ""){
+								
+								vector<string> varToks;
+
+								varToks = splitString(vars[v], ":");
+
+								string varName = varToks[0];
+								string varLine = varToks[1];
+				
+								if(toks[i+1] == varName){
+									num = varLine;
+									break;
+								}
+							}
+						}
+						if(num == ""){
+							num = toks[i+1];
+						}
+
+						int number = atoi(num.c_str());
+
+						cout << opCode << " " << number << endl;
+		
+						translateCodeToBinary(opCode, number);
+
+						break;
+					}
+				}
+			}
+		}
+	}
 	file.close();
 }
 
@@ -44,33 +158,72 @@ void readCode(string fileName){
 void translateCodeToBinary(string opCode, int num){
 	
 	string bitOp;
-	signed int opLen = (sizeof(opcode)/sizeof(opcode[0]));
-
 
 	for( int i=0; i < opLen; i++ ){
 		if( opCode == opcode[i] ){
 			bitOp = opcodeBinary[i];
 			break;
 		}
+	}	
+	
+	//Convert to little endian, binary, and then a string.
+	if(opCode != "STP"){
+		stringstream ss;
+		
+		//~~~~~8 Bit Little Endian Conversion~~~~~//
+
+		uint8_t b = num;
+	
+		b = ((b >> 1) & 0x55555555u) | ((b & 0x55555555u) << 1);
+    		b = ((b >> 2) & 0x33333333u) | ((b & 0x33333333u) << 2);
+    		b = ((b >> 4) & 0x0f0f0f0fu) | ((b & 0x0f0f0f0fu) << 4);	
+
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+		bitset<8> bin_x(b);
+		ss << (bin_x);	
+
+		string bitNum = ss.str();
+
+		cout << bitOp << " " << bitNum << endl;
+		
+		translateBinaryToLine(bitNum, bitOp);
 	}
+	else{
+		cout << bitOp << endl;
+		
+		translateBinaryToLine("00000", bitOp);
+	}	
+}
 
-	//Convert int to byte.
-	unsigned int b = (unsigned int) num;	
-	
-	//Convert byte to little endian, binary, and then a string.
+//Overloaded translation method for dealing with variable declaration.
+void translateCodeToBinary(long int varNum){
+	string varBin;
 	stringstream ss;
+
+	//~~~~~32 Bit Little Endian Conversion~~~~~//
+
+	uint32_t b = varNum;
 	
-	b ^= 0xFFFF;
+	b = ((b >> 1) & 0x55555555u) | ((b & 0x55555555u) << 1);
+    	b = ((b >> 2) & 0x33333333u) | ((b & 0x33333333u) << 2);
+    	b = ((b >> 4) & 0x0f0f0f0fu) | ((b & 0x0f0f0f0fu) << 4);
+    	b = ((b >> 8) & 0x00ff00ffu) | ((b & 0x00ff00ffu) << 8);
+    	b = ((b >> 16) & 0xffffu) | ((b & 0xffffu) << 16);	
+
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+	bitset<32> bin_x(b);
+
+	ss << bin_x;
 	
-	bitset<5> bin_x(b);
-	ss << (bin_x);	
+	varBin = ss.str();
 
-	string bitNum = ss.str();
+	cout << "Generated 32 bit Line:" << endl;
 
-	cout << bitOp << " " << bitNum << endl;
-
-	translateBinaryToLine(bitNum, bitOp);
-
+	cout << varBin << endl << endl;
+	
+	writeMachineCode(varBin);	
 }
 
 //Takes the binary of each lines address and opcode.
@@ -91,7 +244,7 @@ void translateBinaryToLine(string num, string opCode){
 	bLine[15] = opCode[2];
 
 	cout << "Generated 32 bit Line:" << endl;
-	cout << bLine << endl;
+	cout << bLine << endl << endl;
 
 	writeMachineCode(bLine);
 
--- Compiler/compiler.h
+++ Compiler/compiler.h
@@ -1,10 +1,14 @@
 #include <string>
+#include <vector>
 #include <stdlib.h>
 
 using namespace std;
 
+vector<string> splitString(string str, string delims);
+
 void readCode(string fileName);
 void translateCodeToBinary(string opCode, int num);
+void translateCodeToBinary(long int varNum);
 void translateBinaryToLine(string num, string opCode);
 void writeMachineCode(string line);
 
